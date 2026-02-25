70dae8b6d6116a2a09f74efe77c854ead5d3a690
oscar-abud <oscarpalma.vpr2@gmail.com>
2026-02-25 11:55:30 -0300

    feat: add season-harvest view

---
+++ src/pages/SeasonHarvest.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+function SeasonHarvest() {
+  return (
+    <Typography variant="h5" sx={{ color: '#005e4d', fontWeight: 600 }}>
+      Bienvenido a Season Harvest
+    </Typography>
+  )
+}
+
+export default SeasonHarvest
