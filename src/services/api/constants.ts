45f8dde808ff40c9c837bb0655a74219eb075f3d
oscar-abud <oscarpalma.vpr2@gmail.com>
2026-03-13 09:47:01 -0300

    [FEAT] Add url endpoint as variable of const on services/constats.ts

---
+++ src/services/api/constants.ts
@@ -0,0 +1,15 @@
+const apiPrefix = 'api'
+
+/*
+* USUARIO
+*/
+// LOGIN
+export const CONST_ENDPOINT_LOGIN_USUARIO = `${apiPrefix}/usuario/login`;
+// REGISTER
+export const CONST_ENDPOINT_REGISTER_USUARIO = `${apiPrefix}/usuario/register`;
+// UPDATE USUARIO
+export const CONST_ENDPOINT_UPDATE_USUARIO = `${apiPrefix}/usuario/update`;
+
+/*
+* USUARIO
+*/
